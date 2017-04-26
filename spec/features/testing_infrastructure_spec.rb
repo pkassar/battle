1c77d09ff333c9028d4eea1d01840979efd1e4c4
bruxelles86 <w_gant@yahoo.co.uk>
2017-04-26 16:16:18 +0100

    Play page lists score string

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,9 +1,19 @@
-feature 'Testing infrastructure' do
+feature 'entering player names' do
   scenario 'Can add player names' do
     visit('/')
     fill_in('player_1', with: 'Samy')
     fill_in('player_2', with: 'Will')
     click_button('Submit')
     expect(page).to have_content 'Samy VS Will'
+  end
+end
+
+feature 'checking another player\'s points' do
+  scenario 'view player 2\'s hit points' do
+    visit('/')
+    fill_in('player_1', with: 'Samy')
+    fill_in('player_2', with: 'Will')
+    click_button('Submit')
+    expect(page).to have_content '60/60 HP'
   end
 end
