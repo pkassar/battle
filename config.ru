9099bb08b5b2e0959492067827ba709bd9a5d17f
bruxelles86 <w_gant@yahoo.co.uk>
2017-04-27 14:56:16 +0100

    Puts attack method in player class

--- config.ru
+++ config.ru
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'rack'
 
 require_relative './app.rb'
 
--- lib/player.rb
+++ lib/player.rb
@@ -6,4 +6,12 @@ class Player
     @name
     @hp = 100
   end
+
+  def attack(player)
+    player.receive_damage
+  end
+
+  def receive_damage
+    @hp -= 10
+  end
 end
