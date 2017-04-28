101a4e14475902ebeb787ff80de0788955ea73ce
Paul Kassar <paulkassar@gmail.com>
2017-04-28 12:12:26 +0100

    passed our second button attack tests yeyeye

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -15,7 +15,7 @@ end
 feature 'enables player 1 to attack player 2' do
   scenario 'player 1 attacks player 2' do
     sign_in_and_play
-    click_button('Attack Player 2')
+    click_button('Attack Will')
     expect(page).to have_content 'Will Attacked!'
   end
 end
@@ -23,7 +23,7 @@ end
 feature 'enables player 2 to attack player 1' do
   scenario 'player 2 attacks player 1' do
     sign_in_and_play
-    click_button('Attack Player 1')
+    click_button('Attack Samy')
     expect(page).to have_content 'Samy Attacked!'
   end
 end
@@ -31,7 +31,7 @@ end
 feature "I want my attack to reduce Player 2's HP by 10" do
   scenario "player 1 takes 10 hp off player 2's total" do
     sign_in_and_play
-    click_button('Attack')
+    click_button('Attack Will')
     click_button('next turn')
     expect(page).to have_content '90/100'
   end
